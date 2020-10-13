fd345384c8fdd69dc76737ec479f04f45d553c9d
geofr <geofr@Geovanny>
2020-10-13 15:27:38 -0600

    Guardar creando el servlet, direccionamiento no encontrado

---
+++ src/java/beans/Estudiantes.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Emily
+ */
+@Entity
+@Table(name = "ESTUDIANTES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Estudiantes.findAll", query = "SELECT e FROM Estudiantes e"),
+    @NamedQuery(name = "Estudiantes.findById", query = "SELECT e FROM Estudiantes e WHERE e.id = :id"),
+    @NamedQuery(name = "Estudiantes.findByNombre", query = "SELECT e FROM Estudiantes e WHERE e.nombre = :nombre"),
+    @NamedQuery(name = "Estudiantes.findByApellido", query = "SELECT e FROM Estudiantes e WHERE e.apellido = :apellido"),
+    @NamedQuery(name = "Estudiantes.findByCalificacion", query = "SELECT e FROM Estudiantes e WHERE e.calificacion = :calificacion")})
+public class Estudiantes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "NOMBRE")
+    private String nombre;
+    @Size(max = 50)
+    @Column(name = "APELLIDO")
+    private String apellido;
+    @Column(name = "CALIFICACION")
+    private Integer calificacion;
+
+    public Estudiantes() {
+    }
+
+    public Estudiantes(Integer id) {
+        this.id = id;
+    }
+
+    public Estudiantes(Integer id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estudiantes)) {
+            return false;
+        }
+        Estudiantes other = (Estudiantes) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "beans.Estudiantes[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/beans/EstudiantesFacadeLocal.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Emily
+ */
+@Local
+public interface EstudiantesFacadeLocal {
+
+    void create(Estudiantes estudiantes);
+
+    void edit(Estudiantes estudiantes);
+
+    void remove(Estudiantes estudiantes);
+
+    Estudiantes find(Object id);
+
+    List<Estudiantes> findAll();
+
+    List<Estudiantes> findRange(int[] range);
+
+    int count();
+    
+}
